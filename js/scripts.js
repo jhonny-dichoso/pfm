466018d5a54309e9d80834183e939232c3f0d017
Vincent Ramirez <vincent.ramirez@frontlinebusiness.com.ph>
2020-04-24 14:37:46 +0800

    Updated html.

--- js/scripts.js
+++ js/scripts.js
@@ -1,10 +1,22 @@
 const btnDropdown = document.querySelector('.btnDropdown');
 const listMinistries = document.querySelector('.listMinistries');
+const btnBurger = document.querySelector('.btnBurger');
+const nav = document.querySelector('nav');
+// const section = document.querySelectorAll('section').style.zIndex;
 
 btnDropdown.addEventListener('click', () => {
   if (!listMinistries.classList.contains('dropdown')) {
     listMinistries.classList.add('dropdown');
   } else {
     listMinistries.classList.remove('dropdown');
+  }
+});
+
+btnBurger.addEventListener('click', () => {
+  // section.style.zIndex = '1';
+  if (!nav.classList.contains('d-block')) {
+    nav.classList.add('d-block');
+  } else {
+    nav.classList.remove('d-block');
   }
 });

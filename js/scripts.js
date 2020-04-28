40374784a8ad95b8a2ba86fdf568fed10ed9d565
Vincent Ramirez <vincent.ramirez@frontlinebusiness.com.ph>
2020-04-28 13:32:42 +0800

    Fixed mobile responsive sliding banner and added vendor packages.

--- js/scripts.js
+++ js/scripts.js
@@ -2,8 +2,6 @@ const btnDropdown = document.querySelector('.btnDropdown');
 const listMinistries = document.querySelector('.listMinistries');
 const btnBurger = document.querySelector('.btnBurger');
 const nav = document.querySelector('nav');
-// const paymentMethod = document.querySelector('.payment-method');
-// const paymentMethods = document.querySelectorAll('.payment-method');
 
 btnDropdown.addEventListener('click', () => {
   if (!listMinistries.classList.contains('dropdown')) {
@@ -21,38 +19,17 @@ btnBurger.addEventListener('click', () => {
   }
 });
 
-// paymentMethod.addEventListener('click', () => {
-//   for (i = 0; i < paymentMethods.length; i++) {
-//     if (paymentMethods[i].checked) {
-//       console.log(paymentMethods.value);
-
-//       break;
-//     }
-//   }
-// });
-
-// paymentMethod.addEventListener('click', () => {
-//   Array.from(paymentMethods).forEach((methods) => {
-//     console.log(methods.checked);
-//   });
-// });
-
-// paymentMethod.addEventListener('click', () => {
-//   for (i = 0; i < paymentMethod.classList; i++) {
-//     if (paymentMethod.id === 'payment-method-1') {
-//       console.log('payment-method-1 it is!!!');
-//     } else {
-//       console.log('payment-method-2 it is!!!');
-//     }
-//     console.log(paymentMethod);
-//   }
-// });
-
-new Glider(document.querySelector('.glider'), {
-  slidesToShow: 1,
-  slideToScroll: 1,
-  arrows: {
-    prev: '.glider-prev',
-    next: '.glider-next',
-  },
+window.addEventListener('load', function () {
+  new Glider(document.querySelector('.glider'), {
+    slidesToShow: 1,
+    slideToScroll: 1,
+    resizeLock: true,
+    scrollLock: true,
+    draggable: true,
+    dots: '#dots',
+    arrows: {
+      prev: '.glider-prev',
+      next: '.glider-next',
+    },
+  });
 });
